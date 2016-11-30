d8d62bdeab2c926882677381a0467240160af3d3
Roger Rojas <rrojas@hyosystem.com>
2016-11-30 14:28:25 -0500

    Subiendo proyecto

---
+++ app/controllers/productos_controller.rb
@@ -0,0 +1,20 @@
+class ProductosController < ApplicationController
+  def index
+  end
+
+  def new_list( data )
+  	lista = nil
+  	return lista
+  end
+
+  def add(elemento, lista)
+
+  end
+
+  def delete(elemento, lista)
+  end
+
+  def recorrerLista(lista)
+  	
+  end
+end
