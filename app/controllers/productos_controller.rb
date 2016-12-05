80b3a67f0978cc82ea1d4f3dc902ef5be1b0d429
Roger Rojas <roger.rojas.effio@gmail.com>
2016-12-05 02:10:08 -0500

    Seccion productos

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,20 +1,76 @@
 class ProductosController < ApplicationController
+
+
   def index
+    rutaData = "public/data/productos.txt"
+    datos = cargarData( rutaData )
+    data = crearLista( datos )
+    @productos = recorrerLista( data )
   end
 
-  def new_list( data )
-  	lista = nil
-  	return lista
+  def add
+    rutaData = "public/data/productos.txt"
+    datos = cargarData( rutaData )
+    lista = crearLista( datos )
+    if request.post? then
+      codigo = params[:codigo]
+      nombre = params[:descripcion]
+      precio = params[:precio]
+      stock = params[:stock]
+      elemento = codigo.to_s + "," + nombre.to_s + "," + precio.to_s + "," + stock.to_s
+      agregarData(rutaData, elemento)
+      insertarListaFin(lista, elemento)
+    end
   end
 
-  def add(elemento, lista)
-
+  def cargarData( archivo )
+    arch = File.new(archivo, 'r')
+    data = Array.new
+    data = arch.readlines
+    return data
   end
 
-  def delete(elemento, lista)
+  def agregarData( archivo, registro )
+    File.new(archivo, 'w') do |file|
+      file.write "/n" + registro
+    end
+  end
+
+  def crearLista( datos )
+    lista = nil
+    for i in 0...datos.size
+      nodo = [datos[datos.size-1-i],nil]
+      nodo[1] = lista
+      lista = nodo
+    end
+    return lista
+  end
+
+  def insertarListaFin(lista, elemento)
+    nodo = lista
+    if(nodo == nil)
+      lista = elemento
+    else
+      while nodo != nil
+        if nodo[1] == nil then
+          nodo[1] = elemento
+          break
+        else
+          nodo = nodo[1]
+        end
+      end
+    end
+    return lista
   end
 
   def recorrerLista(lista)
-  	
+  	nodo = lista
+    campos = Array.new
+    while nodo != nil
+      fila = nodo[0]
+      campos.push( fila.split(',') )
+      nodo = nodo[1]
+    end
+    return campos
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Rutas para mantenimiento productos
   get 'productos/index'
+  post 'productos/add'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
